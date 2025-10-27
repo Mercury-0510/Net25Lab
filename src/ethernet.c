69f5febcf9b8171cf541240402f86758ff596c04
Rhye7 <caijingyun05@gmail.com>
2025-10-27 16:32:33 +0800

    lab2

--- src/ethernet.c
+++ src/ethernet.c
@@ -10,7 +10,24 @@
  * @param buf 要处理的数据包
  */
 void ethernet_in(buf_t *buf) {
-    // TO-DO
+    if (buf->len < sizeof(ether_hdr_t)) {
+        return;  // 数据包太短，丢弃
+    }
+
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+
+    buf_remove_header(buf, sizeof(ether_hdr_t));
+
+    switch (swap16(hdr->protocol16)) {
+        case NET_PROTOCOL_IP:
+            ip_in(buf, hdr->src);  // 转发给IP协议处理
+            break;
+        case NET_PROTOCOL_ARP:
+            arp_in(buf, hdr->src);  // 转发给ARP协议处理
+            break;
+        default:
+            break;
+    }
 }
 /**
  * @brief 处理一个要发送的数据包
@@ -20,7 +37,23 @@ void ethernet_in(buf_t *buf) {
  * @param protocol 上层协议
  */
 void ethernet_out(buf_t *buf, const uint8_t *mac, net_protocol_t protocol) {
-    // TO-DO
+    if (buf->len < 46) {
+        buf_add_padding(buf, 46 - buf->len);
+    }
+
+    if (buf_add_header(buf, sizeof(ether_hdr_t)) < 0) {
+        return;  // 添加头部失败，返回
+    }
+
+    ether_hdr_t *hdr = (ether_hdr_t *)buf->data;
+
+    memcpy(hdr->dst, mac, NET_MAC_LEN);
+
+    memcpy(hdr->src, net_if_mac, NET_MAC_LEN);
+
+    hdr->protocol16 = swap16(protocol);
+
+    driver_send(buf);
 }
 /**
  * @brief 初始化以太网协议
